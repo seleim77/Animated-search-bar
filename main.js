50a2581326f83d20f309c90308f16a13cc26b818
seleim77 <abdelrahmanselim12345@gmail.com>
2025-09-09 20:42:13 +0300

    finished

---
+++ main.js
@@ -0,0 +1,5 @@
+const searchBarContainer = document.querySelector('.search-bar-container');
+const magnifier = document.querySelector(".magnifier");
+magnifier.addEventListener("click", () => {
+    searchBarContainer.classList.toggle("active");
+})
